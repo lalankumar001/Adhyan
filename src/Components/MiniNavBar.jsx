aa1edf3bc7ae198508efc3c974dce64c8a5cd47d
lalankumar001 <tocsi.org@gmail.com>
2022-05-21 13:38:11 +0530

    htmlproblemfix

--- src/Components/MiniNavBar.jsx
+++ src/Components/MiniNavBar.jsx
@@ -7,6 +7,7 @@ const MiniNavBar = () => {
             <nav className="navbar navbar-expand-lg navbar-info bg-secondary">
                <div className="container">
                    <span className="navbar-brand">
+            
                     <NavLink to="/Contact/BcaAllPage" className="btn btn-">Home</NavLink>
                     <NavLink to="/Contact/BcaAllPage/BcaFirstSem" className="btn btn-">First Sem</NavLink>
                     <NavLink to="/Contact/BcaAllPage/BcaSecondSem"className="btn btn-">Second Sem</NavLink>
@@ -17,6 +18,7 @@ const MiniNavBar = () => {
                     <NavLink to="/Contact/BcaAllPage/BcaAllPracticals" className="btn btn-">Practicals</NavLink>
                     <NavLink to="/Contact/BcaAllPage/BcaPreviousYearQuesPaper" className="btn btn-">Question's paper</NavLink>
                     <NavLink to="/Contact/BcaAllPage/BcaAllMcq" className="btn btn-">MCQ'S</NavLink>
+                
                     </span>
     </div>
 </nav>

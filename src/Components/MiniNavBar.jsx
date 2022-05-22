c17527f9ef56ae96f9da0ec4e6f3d4cef08900a0
lalankumar001 <tocsi.org@gmail.com>
2022-05-22 16:53:06 +0530

    secondupdatemininavbar

--- src/Components/MiniNavBar.jsx
+++ src/Components/MiniNavBar.jsx
@@ -8,16 +8,16 @@ const MiniNavBar = () => {
                <div className="container">
                    <span className="">
             
-                    <NavLink to="/Contact/BcaAllPage" className="btn btn-">Home</NavLink> &nbsp;
-                    <NavLink to="/Contact/BcaAllPage/BcaFirstSem" className="btn btn-">First Sem</NavLink>&nbsp;
-                    <NavLink to="/Contact/BcaAllPage/BcaSecondSem"className="btn btn-">Second Sem</NavLink>&nbsp;
-                    <NavLink to="/Contact/BcaAllPage/BcaThirdSem" className="btn btn-">Third Sem</NavLink>&nbsp;
-                    <NavLink to="/Contact/BcaAllPage/BcaFourthSem" className="btn btn-">Fourth Sem</NavLink>&nbsp;
-                    <NavLink to="/Contact/BcaAllPage/BcaFifthSem"  className="btn btn-">Fifth Sem</NavLink>&nbsp;
-                    <NavLink to="/Contact/BcaAllPage/BcaSixthSem"  className="btn btn-">Sixth Sem</NavLink>&nbsp;
-                    <NavLink to="/Contact/BcaAllPage/BcaAllPracticals" className="btn btn-">Practicals</NavLink>&nbsp;
-                    <NavLink to="/Contact/BcaAllPage/BcaPreviousYearQuesPaper" className="btn btn-">Question's paper</NavLink>&nbsp;
-                    <NavLink to="/Contact/BcaAllPage/BcaAllMcq" className="btn btn-">MCQ'S</NavLink>&nbsp;
+                    <NavLink to="/Contact/BcaAllPage" className="btn btn-">Home</NavLink> &nbsp; &ensp;
+                    <NavLink to="/Contact/BcaAllPage/BcaFirstSem" className="btn btn-">First Sem</NavLink>&nbsp; &ensp;
+                    <NavLink to="/Contact/BcaAllPage/BcaSecondSem"className="btn btn-">Second Sem</NavLink>&nbsp; &ensp;
+                    <NavLink to="/Contact/BcaAllPage/BcaThirdSem" className="btn btn-">Third Sem</NavLink>&nbsp; &ensp;
+                    <NavLink to="/Contact/BcaAllPage/BcaFourthSem" className="btn btn-">Fourth Sem</NavLink>&nbsp; &ensp;
+                    <NavLink to="/Contact/BcaAllPage/BcaFifthSem"  className="btn btn-">Fifth Sem</NavLink>&nbsp; &ensp;
+                    <NavLink to="/Contact/BcaAllPage/BcaSixthSem"  className="btn btn-">Sixth Sem</NavLink>&nbsp; &ensp;
+                    <NavLink to="/Contact/BcaAllPage/BcaAllPracticals" className="btn btn-">Practicals</NavLink>&nbsp; &ensp;
+                    <NavLink to="/Contact/BcaAllPage/BcaPreviousYearQuesPaper" className="btn btn-">Question's paper</NavLink>&nbsp; &ensp;
+                    <NavLink to="/Contact/BcaAllPage/BcaAllMcq" className="btn btn-">MCQ'S</NavLink>
                 
                     </span>
     </div>

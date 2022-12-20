15ac00a38b4c8688a97a922ee58f32c673143ea3
lalankumar001 <tocsi.org@gmail.com>
2022-12-20 19:06:57 +0530

    navcommit

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,44 +5,6 @@ import Menu from './Images/Menu.svg'
 
 
 const Login = () => {
-<<<<<<< Updated upstream
-    return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-            <div className="container-fluid">
-             <NavLink className="navbar-brand" to="/#"></NavLink>
-             <img  className="logo" src={Logo} alt="Logo" />
-             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-               <span className="navbar-toggler-icon"></span>
-             </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <NavLink  activeClassName="menu_active" className="nav-link"  id='NavBarLinkFirst' to="/Home">Home</NavLink> 
-        </li>
-        <li className="nav-item">
-          <NavLink activeClassName="menu_active" className="nav-link" id='NavBarLinkSecond'  to="/Contact">Contact Us</NavLink> 
-        </li>
-        {/* <li className="nav-item">
-          <NavLink  activeClassName="menu_active"className="nav-link" to="/Login">Login</NavLink>
-        </li>
-        <li className="nav-item">
-        <NavLink  activeClassName="menu_active" className="nav-link" to="/Register">Register</NavLink>
-        </li> */}
-  <li className="nav-item">
-        <NavLink  activeClassName="menu_active" className="nav-link" id='NavBarLinkThird'  to="/About">About Us</NavLink> 
-        </li>
-<li className="nav-item">
-        <NavLink  activeClassName="menu_active" className="nav-link" id='NavBarLinkThird'  to="/About">Blog</NavLink> 
-        </li>
-<li className="nav-item">
-        <NavLink  activeClassName="menu_active" className="nav-link" id='NavBarLinkThird'  to="/About">Login/Register</NavLink> 
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-=======
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-light fixed-top">
@@ -112,7 +74,6 @@ const Login = () => {
             </div>
 
           </div>
->>>>>>> Stashed changes
         </div>
       </nav>
     </div>

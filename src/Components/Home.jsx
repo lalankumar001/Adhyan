a08b9fd5b9cfd2a0b55aed58a231f9aa48412ee6
lalankumar001 <tocsi.org@gmail.com>
2022-12-20 17:56:27 +0530

    Revert "NavbarFixed"
    This reverts commit 7b522b6bf6c34455da9042ec0823f4b274624ce3.

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,56 +1,37 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import Hero from './Images/HomeHero.svg'
-import Instagram from './Images/instagra.png'
-import Linkdin from './Images/linkdin.png'
-import Twitter from './Images/twitter.png'
-import Youtube from './Images/youtube.png'
-import Footer from '../Components/AllPageFooter'
+import { AiOutlineArrowDown } from "react-icons/ai";
+import pic03 from './Images/HomepagePIc.png'
 const Home = () => {
     return (
-          <div>
-        <div className='mt-5 p-5'>
-
-            <div class="container text-center">
-                <div class="row row-cols-2" id='HeroSection'>
-                    {/* Home hero text section */}
-                    <div class="col mt-5 p-5" id='HeroTextSection'>
-                        <h1 id='HeroText' className='mt-3 p-2 text-start sm:text-start'>Welcome to Adhyan</h1>
-                        <h3 className='text-start fw-bold sm: mt-1 p-1 text-start'>Unlock Your Learning Potential with <b className='text-info'>us!</b></h3>
-                        {/* Home hero button section */}
-                        <div className="col text-start mt-5 ">
-                            <button className='btn btn-dark'> <NavLink activeClassName="menu_active" className="nav-link" to="/Contact/BcaAllPage">View Courses</NavLink></button>
-                            <button className='btn btn-light border '> <NavLink activeClassName="menu_active" className="nav-link" to="/Contact/BcaAllPage">Explore</NavLink></button>
-                        </div>
-                    </div>
-                    {/* Home hero img section */}
-                    <div class="col">
-                        <img src={Hero} alt="" className='mt-2 w-100 h-100 p-3 ' id='HeroImgSection' />
-                    </div>
-                </div>
-            </div>
-        </div>
-
-             <div class="container w-100 text-center ">
-  <div class="row">
-    <h1 className='fs-1 fw-bolder mb-5'>Follow us</h1>
-    <div class="col">
-      <a href="https://www.instagram.com/lalan_kumar001/" target="_blank"><img src={Instagram} alt="" className='w-75 h-75' /></a>
-    </div>
-    <div class="col">
-     <a href="https://www.linkedin.com/in/lalan-kumar52526/" target="_blank"> <img src={Linkdin} alt="linkedinImg"className='w-75 h-75' /></a>
-    </div>
-    <div class="col">
-     <a href="https://twitter.com/Lalan_kumar001?t=YztJYDa2hfvp60Bz0DrRmg&s=08" target="_blank"> <img src={Twitter} alt="" className='w-75 h-75'/></a>
-    </div>
-    <div class="col">
-     <a href="https://www.youtube.com/channel/UCGoQTxp8Vd17oxUM7iNJ8dA" target="_blank"> <img src={Youtube} alt="YoutubeImg" className='w-75 h-75'/></a>
-    </div>
+         <div id='main'>
+<header class="site-header clearfix">
+ <section>
+  <div class="leftside"> 
+   <img src={pic03} className="img-thumbnail " alt='ProfilePic'></img>
   </div>
-</div>
-        <Footer />
+  <div class="rightside"> 
+    <h1>Hey Folks.. </h1>
+   <h1>Welcome To The Adhyan</h1>
+   <p>The Website For Complete Study Material and many more </p>
+   <h2>Courses  <AiOutlineArrowDown /> </h2>
+   {/* <span className=''>
+ <button className='first_btn'> <NavLink  activeClassName="menu_active" className="nav-link"  to="/About">About Me</NavLink></button>
+ <button className='second_btn'> <NavLink activeClassName="menu_active" className="nav-link" to="/Projects">Projects</NavLink> </button>
+ </span> */}
+  </div>  
+ </section>
+</header>
+<span className='rightside_btn'>
+ <button className='first_btn'> <NavLink  activeClassName="menu_active" className="nav-link"  to="/Contact/BcaAllPage">BCA</NavLink></button>
+ <button className='second_btn'> <NavLink activeClassName="menu_active" className="nav-link" to="/Contact/BtechAllPage">MCA</NavLink> </button>
+ <button className='second_btn'> <NavLink activeClassName="menu_active" className="nav-link" to="/Contact/BtechAllPage">BTECH</NavLink> </button>
+ <button className='second_btn'> <NavLink activeClassName="menu_active" className="nav-link" to="/Contact/BtechAllPage">MTECH</NavLink> </button>
+ </span>
+{/* <SocialMediaIcons /> */}
         </div>
-
+      
+        
     )
 }
 
